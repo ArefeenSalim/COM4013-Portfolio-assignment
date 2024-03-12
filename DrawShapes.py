cf78ec5aa97a40a85b70897087e903e1eda011c6
ArefeenSalime <2303968@leedstrinity.ac.uk>
2024-03-12 12:42:26 +0000

    erstyuiopiuytrerty

--- DrawShapes.py
+++ DrawShapes.py
@@ -11,39 +11,69 @@ print ("8 ")
 
 def Square():
     n = int(input("Enter the size of the square: "))
-    for i in range(0, n):
-        for j in range(0, n): 
-            print("*", end="")
-    print()
+    for i in range(n):
+        print("* "*n)
 
 def Triangle():
+    n = int(input("Enter the height of the triangle: "))
+    for i in range(1, n + 1):
+        print(" "* (n - i) + "* " * i)
+
+def RightAngledTriangle():
+    n = int(input("Enter the height of the right angled triangle: "))
+    for i in range(n):
+        for j in range(i + 1):
+            print("* ", end="")
+        print()
+
+def Paralellogram():
+    n = int(input("Enter the height of the paralellogram: "))
+    m = int(input("Enter the length of the base: "))
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end="")
+        for j in range(m):
+            print("* ", end="")
+        print() 
+
+def Rectangle():
+    n = int(input("Enter the height of the rectangle: "))
+    m = int(input("Enter the length of the base: "))
+    for i in range(n):
+        for j in range(m):
+            print("* ", end="")
+        print()
 
 def Shape():
     while True:
-        shape = input ("Enter the number of the shape")
+        shape = input ("Enter the number of the shape: ")
         if shape == "1":
             print (" 1 Square")
             Square()
             break
-        #elif shapeType== "2":
-            #print("2 Triangle")
-            #break
-        #elif shapeType== "3":
-            #print("3 Right Angled Triangle")
-           # break
-        #elif shapeType== "4":
-           # print("4 Paralellogram")
-            #break
-        #elif shapeType== "5":
-            #print("5 Rectangle")
-            #break
-        #elif shapeType== "6":
+        elif shape == "2":
+            print("2 Triangle")
+            Triangle()
+            break
+        elif shape== "3":
+            print("3 Right Angled Triangle")
+            RightAngledTriangle()
+            break
+        elif shape == "4":
+            print("4 Paralellogram")
+            Paralellogram()
+            break
+        elif shape == "5":
+            print("5 Rectangle")
+            Rectangle()
+            break
+        #elif shape == "6":
             #print("6")
             #break
-        #elif shapeType== "7":
+        #elif shape == "7":
             #print("7")
             #break
-        #elif shapeType== "8":
+        #elif shape == "8":
             #print("8")
             #break
         else: 
@@ -51,5 +81,4 @@ def Shape():
 
 def main():
     Shape()
-    Square()
 main()    
